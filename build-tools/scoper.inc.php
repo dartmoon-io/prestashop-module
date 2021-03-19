03c7a2e997d14157fd76299eb7d4859ed258e5d0
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2021-03-19 10:17:34 +0100

    Updated dartmoon/prestashop-build-tools

---
+++ build-tools/scoper.inc.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+use Isolated\Symfony\Component\Finder\Finder;
+
+return [
+    // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
+    // directory. You can however define which files should be scoped by defining a collection of Finders in the
+    // following configuration key.
+    //
+    // For more see: https://github.com/humbug/php-scoper#finders-and-paths
+    'finders' => [
+        Finder::create()
+            ->files()
+            ->in('vendor')
+            ->exclude([
+                'bin',
+                'composer',
+            ])
+            ->name([
+                '*.php', 
+                'LICENSE', 
+                'composer.json'
+            ])
+            ->notName(['autoload.php']),
+        Finder::create()
+            ->append(['vendor/build-tools.txt']),
+    ],
+
+    // Whitelists a list of files. Unlike the other whitelist related features, this one is about completely leaving
+    // a file untouched.
+    // Paths are relative to the configuration file unless if they are already absolute
+    'files-whitelist' => [
+        //
+    ],
+
+    // When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
+    // original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
+    // support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
+    // heart contents.
+    //
+    // For more see: https://github.com/humbug/php-scoper#patchers
+    'patchers' => [],
+
+    // PHP-Scoper's goal is to make sure that all code for a project lies in a distinct PHP namespace. However, you
+    // may want to share a common API between the bundled code of your PHAR and the consumer code. For example if
+    // you have a PHPUnit PHAR with isolated code, you still want the PHAR to be able to understand the
+    // PHPUnit\Framework\TestCase class.
+    //
+    // A way to achieve this is by specifying a list of classes to not prefix with the following configuration key. Note
+    // that this does not work with functions or constants neither with classes belonging to the global namespace.
+    //
+    // Fore more see https://github.com/humbug/php-scoper#whitelist
+    'whitelist' => [
+        'PrestaShop\PrestaShop\Adapter\Entity\*',
+        'PrestaShopBundle\Entity\Repository\*',
+        'PrestaShop\PrestaShop\Adapter\*',
+    ],
+
+    // If `true` then the user defined constants belonging to the global namespace will not be prefixed.
+    //
+    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
+    'whitelist-global-constants' => true,
+
+    // If `true` then the user defined classes belonging to the global namespace will not be prefixed.
+    //
+    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
+    'whitelist-global-classes' => true,
+
+    // If `true` then the user defined functions belonging to the global namespace will not be prefixed.
+    //
+    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
+    'whitelist-global-functions' => true,
+];
