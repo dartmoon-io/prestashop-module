ddecae84963ae0c20c1c75a79334db1dba17fab9
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2023-09-22 17:22:22 +0200

    Fixed licenses

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,14 @@
 let mix = require('laravel-mix');
 
+mix.options({
+   terser: {
+     extractComments: false,
+   },
+   cssnano: {
+      discardComments: { removeAll: true }
+   }
+});
+
 mix.js('_dev/js/front.js', 'js/front.js')
    .js('_dev/js/back.js', 'js/back.js')
    .sass('_dev/scss/front.scss', 'css/front.css')
